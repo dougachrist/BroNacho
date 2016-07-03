1e459c641a43d1327bd1745a68ca7d3d770bbb45
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-07-03 13:49:24 -0700

    Updated Button

---
+++ results.js
@@ -0,0 +1,4 @@
+'use strict';
+localStorage.getItem('top3BroNachos',JSON.parse(top3BroNachosObjArray[0]).filePath);
+localStorage.getItem('top3BroNachos',JSON.parse(top3BroNachosObjArray[1]).filePath);
+localStorage.getItem('top3BroNachos',JSON.parse(top3BroNachosObjArray[2]).filePath);
