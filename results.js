b34be0e9207335dc4b1db537145e329589fd9f8c
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-07-03 14:58:12 -0700

    Added About Page

--- results.js
+++ results.js
@@ -1,4 +1,6 @@
 'use strict';
-localStorage.getItem('top3BroNachos',JSON.parse(top3BroNachosObjArray[0]).filePath);
+document.getElementById('BroNacho1');
+BroNacho1.textContent = localStorage.getItem('top3BroNachos',JSON.parse(top3BroNachosObjArray[0]).filePath);
+
 localStorage.getItem('top3BroNachos',JSON.parse(top3BroNachosObjArray[1]).filePath);
 localStorage.getItem('top3BroNachos',JSON.parse(top3BroNachosObjArray[2]).filePath);
