a9a51832a655bf26c5c5b6c2fe8bfbb31310810e
Sean Thomas Anderson <Anderson.Sean27@gmail.com>
2016-07-04 12:07:29 -0700

    Minor changes. Not working.

--- results/results.js
+++ results/results.js
@@ -2,15 +2,17 @@
 
 var top3BroNachosArray = JSON.parse(localStorage.getItem('top3BroNachos'));
 
-
 var ImgEl = document.getElementById('BroNacho1');
 ImgEl.setAttribute('src',top3BroNachosArray[0].filePath);
-
 var pEl = document.getElementById('BroNacho1Name');
 pEl.textContent = top3BroNachosArray[0].userName;
 
-// imageObjectArray[w] = JSON.parse(localStorage.getItem(imageObjectArray[w].imgName));
-//
-//
-// localStorage.getItem('top3BroNachos',JSON.parse(top3BroNachosObjArray[1]).filePath);
-// localStorage.getItem('top3BroNachos',JSON.parse(top3BroNachosObjArray[2]).filePath);
+var ImgEl2 = document.getElementById('BroNacho2');
+ImgEl2.setAttribute('src',top3BroNachosArray[1].filePath);
+var pEl2 = document.getElementById('BroNacho2Name');
+pEl2.textContent = top3BroNachosArray[1].userName;
+
+var ImgEl3 = document.getElementById('BroNacho3');
+ImgEl3.setAttribute('src',top3BroNachosArray[2].filePath);
+var pEl3 = document.getElementById('BroNacho3Name');
+pEl3.textContent = top3BroNachosArray[2].userName;
